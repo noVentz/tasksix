64a89846fca0983879d886e0e337fb8770197b38
noVentz <103551295+noVentz@users.noreply.github.com>
2022-04-13 21:54:01 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TaskSix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 3, allPicture = 52;
+            int fullRow = allPicture/row;
+            int remainsPicture = allPicture % row;
+            Console.WriteLine("Количество полностью заполненных рядов {0} \n Количество картионк сверх меры {1}", fullRow,remainsPicture);
+        }
+    }
+}
